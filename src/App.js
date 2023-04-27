e3b66ffab17b0514f486a9c6b7a89a1d8b40182f
Mac mini 1 <macmini1@Macs-Mac-mini.local>
2023-04-27 10:18:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [mobile, setMobile] = useState("");
+
+  function saveUser() {
+    console.log(name, email, mobile);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>User form</h1>
+      <input type="text" name="name" value={name} onChange={(e)=> {setName(e.target.value)}}/><br />
+      <input type="text" name="email" value={email} onChange={(e)=> {setEmail(e.target.value)}} /><br />
+      <input type="text" name="mobile" value={mobile} onChange={(e)=> {setMobile(e.target.value)}}/><br />
+      <button type="button" onClick={saveUser}>Save User</button>
     </div>
   );
 }
